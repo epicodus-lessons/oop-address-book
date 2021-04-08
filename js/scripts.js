8463a73d1483c0ae161ccee3891f0ab19d2753b0
Franz Knupfer <franzknupfer@hotmail.com>
2021-04-08 09:41:24 -0700

    Add semis for consistency

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,12 @@ function AddressBook() {
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
-}
+};
 
 AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
-}
+};
 
 // Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
@@ -23,4 +23,4 @@ function Contact(firstName, lastName, phoneNumber) {
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
-}
+};
